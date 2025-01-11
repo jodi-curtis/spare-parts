e64148277c23e005e98dce0855b6ca6c203ae150
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2025-01-11 22:43:15 +0000

    Update to PartListView

--- parts/urls.py
+++ parts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PartListView
 
 urlpatterns = [
     path('', views.home, name='parts-home'),
-    path('list/', views.parts, name='parts-list'),
+    path('list/', PartListView.as_view(), name='parts-list'),
 ]
--- parts/views.py
+++ parts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . models import Part
+from django.views.generic import ListView
 from django.db.models import Q
 
 # Create your views here.
@@ -12,22 +13,25 @@ def home(request):
     }
     return render(request, 'parts/home.html', context)
 
-@login_required
-def parts(request):
-    #Get search value from search bar
-    search_value = request.GET.get('searchVal','')
-    #If search value is set
-    if search_value:
-        #Filter parts list where code or name contains the search value
-        parts = Part.objects.filter(Q(code__icontains=search_value) | Q(name__icontains=search_value))
-    #If no search value set
-    else:
-        #Get all parts
-        parts = Part.objects.all()
-    
-    context = {
-        'title': 'Spare Parts',
-        'parts': parts,
-        'search_value': search_value 
-    }
-    return render(request, 'parts/parts.html', context)
+class PartListView(ListView):
+    model = Part
+    context_object_name = 'parts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Get seach value, if not set set as empty
+        search_value = self.request.GET.get('searchVal', '')
+        
+        # If search value exists, filter parts where code or name contains search value
+        if search_value:
+            queryset = queryset.filter(Q(code__icontains=search_value) | Q(name__icontains=search_value))
+        
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Add search value to context
+        context['search_value'] = self.request.GET.get('searchVal', '')
+        
+        return context
