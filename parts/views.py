d2490e6bec43a492782831a5318b3f5d1aa6e121
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2025-01-11 22:06:12 +0000

    Add search to parts list

--- parts/views.py
+++ parts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . models import Part
-
+from django.db.models import Q
 
 # Create your views here.
 @login_required
@@ -14,8 +14,20 @@ def home(request):
 
 @login_required
 def parts(request):
+    #Get search value from search bar
+    search_value = request.GET.get('searchVal','')
+    #If search value is set
+    if search_value:
+        #Filter parts list where code or name contains the search value
+        parts = Part.objects.filter(Q(code__icontains=search_value) | Q(name__icontains=search_value))
+    #If no search value set
+    else:
+        #Get all parts
+        parts = Part.objects.all()
+    
     context = {
         'title': 'Spare Parts',
-        'parts': Part.objects.all() 
+        'parts': parts,
+        'search_value': search_value 
     }
     return render(request, 'parts/parts.html', context)
