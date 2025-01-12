9a64801b159ded15188b8ab365752d8462eed4ff
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2025-01-12 00:11:19 +0000

    Added part detail page

--- parts/urls.py
+++ parts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PartListView
+from .views import PartListView, PartDetailView
 
 urlpatterns = [
     path('', views.home, name='parts-home'),
     path('list/', PartListView.as_view(), name='parts-list'),
+    path('list/<int:pk>', PartDetailView.as_view(), name='part-detail'),
 ]
--- parts/views.py
+++ parts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . models import Part
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Q
 
 # Create your views here.
@@ -21,17 +21,16 @@ class PartListView(ListView):
         queryset = super().get_queryset()
         # Get seach value, if not set set as empty
         search_value = self.request.GET.get('searchVal', '')
-        
         # If search value exists, filter parts where code or name contains search value
         if search_value:
             queryset = queryset.filter(Q(code__icontains=search_value) | Q(name__icontains=search_value))
-        
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
         # Add search value to context
         context['search_value'] = self.request.GET.get('searchVal', '')
-        
         return context
+    
+class PartDetailView(DetailView):
+    model = Part
