86f4055f7199c84c29f618443fe30a1d27d3cae2
jodi-curtis <jodi.curtis@mannokbuild.com>
2025-06-05 10:30:10 +0100

    Test remove Parts folder
    Test removing Parts folder so gitignore works correctly

--- parts/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import Part
-from django.db.models import F
-
-def low_stock_count(request):
-    # Count the number of parts which are low in stock
-    count = Part.objects.filter(num_in_stock__lte=F('low_stock_warning')).count()
-    return {'low_stock_count': count}
